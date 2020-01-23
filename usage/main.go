48c71edda4ebbbc2d8a0a5114e62ac19725dcd26
Guillaume Coré <gucore@redhat.com>
2020-01-23 12:16:06 +0100

    WIP

--- usage/main.go
+++ usage/main.go
@@ -5,13 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-var sess client.ConfigProvider
 var maxRetries = 100
 
 func buildTypeMap(types *ec2.DescribeInstanceTypesOutput) map[string] int64 {
@@ -36,7 +34,52 @@ func buildTypeMap(types *ec2.DescribeInstanceTypesOutput) map[string] int64 {
 
 var stats = map[string] int64 {}
 
-func capture(
+func captureVolumes(region string, result *ec2.DescribeVolumesOutput) {
+	for _, volume := range result.Volumes {
+		for _, prefix := range []string {"total.", region + "."} {
+			stats[prefix + "volumes.size_gib"] = stats[prefix + "volumes.size_gib"] + *volume.Size
+			stats[prefix + "volumes.count"] = stats[prefix + "volumes.count"] + 1
+		}
+	}
+}
+
+func getVolumes(svc *ec2.EC2) *ec2.DescribeVolumesOutput {
+	result, err := svc.DescribeVolumes(&ec2.DescribeVolumesInput{})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		fmt.Println(result)
+	}
+	return result
+}
+
+func getAddresses(svc *ec2.EC2) *ec2.DescribeAddressesOutput {
+	result, err := svc.DescribeAddresses(&ec2.DescribeAddressesInput{})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		fmt.Println(result)
+	}
+	return result
+}
+
+func captureInstances(
 	region string,
 	result *ec2.DescribeInstancesOutput,
 	types *ec2.DescribeInstanceTypesOutput,
@@ -48,7 +91,8 @@ func capture(
 		for _, instance := range reservation.Instances {
 			for _, preprefix := range []string {"total.", region + "."} {
 				prefix := preprefix + *states[0] + "."
-				stats[prefix + "instance." + *instance.InstanceType] = stats[prefix + "instance." + *instance.InstanceType] + 1
+				stats[prefix + "instances"] = stats[prefix + "instances"] + 1
+				stats[prefix + "instances." + *instance.InstanceType] = stats[prefix + "instances." + *instance.InstanceType] + 1
 
 				stats[prefix + "vcpus"] = stats[prefix + "vcpus"] + tm[*instance.InstanceType + ".vcpus"]
 				stats[prefix + "cores"] = stats[prefix + "cores"] + tm[*instance.InstanceType + ".cores"]
@@ -58,16 +102,7 @@ func capture(
 	}
 }
 
-func getInstances(region *string, states []*string) *ec2.DescribeInstancesOutput {
-	sess, _ := session.NewSession(
-		&aws.Config{
-			Region:     region,
-			MaxRetries: &maxRetries,
-		},
-	)
-
-	svc := ec2.New(sess)
-
+func getInstances(svc *ec2.EC2, states []*string) *ec2.DescribeInstancesOutput {
 	input := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			{
@@ -77,7 +112,14 @@ func getInstances(region *string, states []*string) *ec2.DescribeInstancesOutput
 		},
 	}
 
-	instances, err := svc.DescribeInstances(input)
+	instances := []*DescribeInstancesOutput{}
+	err := svc.DescribeInstances(input,
+		func(page *ec2.DescribeInstanceOutput, lastePage bool) bool {
+			instances = instances
+
+		}
+
+	)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
@@ -103,7 +145,7 @@ func main() {
 		os.Setenv("AWS_REGION", "us-east-1")
 	}
 
-	sess, _ = session.NewSession(
+	sess, _ := session.NewSession(
 		&aws.Config{
 			Region:     aws.String(os.Getenv("AWS_REGION")),
 			MaxRetries: &maxRetries,
@@ -116,22 +158,37 @@ func main() {
 	regions, _ := svcGlob.DescribeRegions(&ec2.DescribeRegionsInput{})
 
 	for _, region := range regions.Regions {
+		if *region.RegionName != "eu-central-1" {
+			continue
+		}
+		sess, _ := session.NewSession(
+			&aws.Config{
+				Region:     region.RegionName,
+				MaxRetries: &maxRetries,
+			},
+		)
+		svc := ec2.New(sess)
+
+		volumes := getVolumes(svc)
+		captureVolumes(*region.RegionName, volumes)
+
 		states := []*string{
 			aws.String("running"),
 			aws.String("pending"),
 		}
-		instances := getInstances(region.RegionName, states)
-		capture(*region.RegionName, instances, types, states)
+		instances := getInstances(svc, states)
+		captureInstances(*region.RegionName, instances, types, states)
 
 		states = []*string{
 			aws.String("stopped"),
 			aws.String("shutting-down"),
 			aws.String("stopping"),
 		}
-		instances = getInstances(region.RegionName, states)
-		capture(*region.RegionName, instances, types, states)
+		instances = getInstances(svc, states)
+		captureInstances(*region.RegionName, instances, types, states)
 	}
 
 	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", " ")
 	enc.Encode(&stats)
 }
